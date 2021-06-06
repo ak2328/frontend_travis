2eb157a5104640187b92758ed776232cf31a2b3b
root <root@ip-192-168-45-14.ap-south-1.compute.internal>
2021-06-06 22:27:57 +0000

    updated code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Lets check Travis!
+          Deployment Completed!
         </p>
         <a
           className="App-link"
