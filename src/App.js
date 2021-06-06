48875b00fc47e6d6a1d6d3a38ab7787f46c9e884
root <root@ip-192-168-45-14.ap-south-1.compute.internal>
2021-06-06 22:12:26 +0000

    added something

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Back to Good Work!
+          Check Deployment!
         </p>
         <a
           className="App-link"
