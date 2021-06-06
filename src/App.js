a45fbfee3c80a654447a2089c40fe60a809fc1f7
root <root@ip-192-168-45-14.ap-south-1.compute.internal>
2021-06-06 22:14:49 +0000

    added travis file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Check Deployment!
+          Lets check Travis!
         </p>
         <a
           className="App-link"
